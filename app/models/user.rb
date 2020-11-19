9b0a978812364d07b0ebf1cd3dfae448dfaada09
KakudaYutaka <73814373+KakudaYutaka@users.noreply.github.com>
2020-11-20 02:58:49 +0900

    ユーザー登録文字制限実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,        presence: true
-  validates :last_name,       presence: true
-  validates :first_name,      presence: true
-  validates :kana_last_name,  presence: true
-  validates :kana_first_name, presence: true
+  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true }
+  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true }
+  validates :kana_last_name,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true }
+  validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true }
   validates :birth,           presence: true
+
+  validates :password, format: { with: /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/, allow_blank: true }
 end
