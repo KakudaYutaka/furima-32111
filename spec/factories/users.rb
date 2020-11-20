5fd3b92ef051afec85ffed0a791f5dc938ac7f4f
KakudaYutaka <73814373+KakudaYutaka@users.noreply.github.com>
2020-11-20 16:07:28 +0900

    FactoryBot作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,15 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.last_name}
+    nickname {Faker::Name.name}
     email {Faker::Internet.free_email}
     password = Faker::Internet.password(min_length: 6)
     password {password}
     password_confirmation {password}
+    person = Gimei.name
+    last_name { person.last.kanji }
+    first_name { person.first.kanji }
+    kana_last_name { person.last.hiragana }
+    kana_first_name { person.first.hiragana }
+    birth {Faker::Date.birthday}
   end
 end
