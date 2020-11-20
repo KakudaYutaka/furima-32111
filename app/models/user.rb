5fb4da7464f4eb864a007edd446960c709ca7810
KakudaYutaka <73814373+KakudaYutaka@users.noreply.github.com>
2020-11-20 15:30:07 +0900

    テストコード準備エラー文記述忘れ

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,        presence: true
-  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true }
-  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true }
-  validates :kana_last_name,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true }
-  validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true }
+  validates :nickname,  presence: true
+  validates :email,     uniqueness: true
+  validates :password,  format: { with: /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/, allow_blank: true, message: "Include both letters and numbers" }
+  validates :last_name,
+    presence: true,
+    format: { with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true, message: "Full-width characters" }
+  validates :first_name,
+    presence: true,
+    format: { with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true, message: "Full-width characters" }
+  validates :kana_last_name,
+    presence: true,
+    format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true, message: "Full-width katakana characters" }
+  validates :kana_first_name,
+    presence: true,
+    format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true, message: "Full-width katakana characters" }
   validates :birth,           presence: true
-
-  validates :password, format: { with: /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/, allow_blank: true }
 end
