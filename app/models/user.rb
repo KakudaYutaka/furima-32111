d6b749670bd538e640f89a808ccd7dd94dd78e28
KakudaYutaka <73814373+KakudaYutaka@users.noreply.github.com>
2020-11-21 00:55:38 +0900

    presence: true纏め、テストコード'emailに＠が含まれていないと登録できない追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,  presence: true
-  validates :email,     uniqueness: true
-  validates :password,  format: { with: /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/, allow_blank: true, message: 'Include both letters and numbers' }
-  validates :last_name,
-            presence: true,
-            format: { with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true, message: 'Full-width characters' }
-  validates :first_name,
-            presence: true,
-            format: { with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true, message: 'Full-width characters' }
-  validates :kana_last_name,
-            presence: true,
-            format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true, message: 'Full-width katakana characters' }
-  validates :kana_first_name,
-            presence: true,
-            format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true, message: 'Full-width katakana characters' }
-  validates :birth, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :email,           uniqueness: true
+    validates :password,        format: { with: /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/, allow_blank: true, message: 'Include both letters and numbers' }
+    validates :last_name,       format: { with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true, message: 'Full-width characters' }
+    validates :first_name,      format: { with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true, message: 'Full-width characters' }
+    validates :kana_last_name,  format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true, message: 'Full-width katakana characters' }
+    validates :kana_first_name, format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true, message: 'Full-width katakana characters' }
+    validates :birth
+  end
 end
