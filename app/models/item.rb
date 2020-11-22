c99fc167b3a917c94df5c26ba85e229a44f77928
KakudaYutaka <73814373+KakudaYutaka@users.noreply.github.com>
2020-11-22 22:19:55 +0900

    価格半角数字以外エラー表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,15 @@ class Item < ApplicationRecord
     validates :ship_method_id
     validates :shipment_source_id
     validates :days_to_ship_id
-    validates :price #価格の範囲指定 半角数字のみ機能追加すること
+    validates :price, numericality: { only_integer: true, allow_blank: true, message: 'Half-width number' }#価格の範囲機能追加すること
+  end
+
+  with_options numericality: { other_than: 1, message: 'Select' } do
+    validates :category_id
+    validates :status_id
+    validates :ship_method_id
+    validates :shipment_source_id
+    validates :days_to_ship_id
   end
 
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -20,4 +28,6 @@ class Item < ApplicationRecord
   belongs_to_active_hash :ship_method
   belongs_to_active_hash :shipment_source
   belongs_to_active_hash :days_to_ship
+
+
 end
