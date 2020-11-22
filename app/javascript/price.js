f8be2264449846e43c48cd009b3848baf16c0693
KakudaYutaka <73814373+KakudaYutaka@users.noreply.github.com>
2020-11-23 01:32:06 +0900

    価格イベント発火修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,7 +1,7 @@
 function calc (){
   const itemPrice = document.getElementById("item-price");
 
-  itemPrice.addEventListener("keyup", () => {
+  itemPrice.addEventListener("input", () => {
     const addTaxPrice = document.getElementById("add-tax-price");
     const salesProfit = document.getElementById("profit");
     taxPrice = Math.floor(itemPrice.value * 0.1)/ 1;
