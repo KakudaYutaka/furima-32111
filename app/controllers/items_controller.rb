e513f847710061e06c3d1f1d1f1e190108d98d57
KakudaYutaka <73814373+KakudaYutaka@users.noreply.github.com>
2020-11-22 22:46:42 +0900

    ストロングパロメーター

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,19 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:product, :description, :category_id, :status_id, :ship_method_id, :shipment_source_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
