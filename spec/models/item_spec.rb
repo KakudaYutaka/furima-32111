433b1214079516eb4743c894f9789ed7a1f2dab2
KakudaYutaka <73814373+KakudaYutaka@users.noreply.github.com>
2020-11-23 18:42:14 +0900

    コードレビュー指摘箇所修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,10 +9,6 @@ describe Item do
       it 'image,product,description,category_id,status_id,ship_method_id,shipment_source_id,days_to_ship_id,price,全てが存在すれば登録できる' do
         expect(@item).to be_valid
       end
-      it 'priceが半角数字であれば登録できる' do
-        @item.price = '100000'
-        expect(@item).to be_valid
-      end
     end
 
     context '商品登録がうまくいかないとき' do
@@ -31,28 +27,28 @@ describe Item do
         @item.valid?
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
-      it 'category_idが空だと登録できない' do
-        @item.category_id = ''
+      it 'category_idが1（--）の場合は登録できない' do
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Category Select')
       end
-      it 'status_idが空だと登録できない' do
-        @item.status_id = ''
+      it 'status_idが1（--）の場合は登録できない' do
+        @item.status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Status Select')
       end
-      it 'ship_method_idが空だと登録できない' do
-        @item.ship_method_id = ''
+      it 'ship_method_idが1（--）の場合は登録できない' do
+        @item.ship_method_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Ship method Select')
       end
-      it 'shipment_source_idが空だと登録できない' do
-        @item.shipment_source_id = ''
+      it 'shipment_source_idが1（--）の場合は登録できない' do
+        @item.shipment_source_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Shipment source Select')
       end
-      it 'days_to_ship_idが空だと登録できない' do
-        @item.days_to_ship_id = ''
+      it 'days_to_ship_idが1（--）の場合は登録できない' do
+        @item.days_to_ship_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include('Days to ship Select')
       end
