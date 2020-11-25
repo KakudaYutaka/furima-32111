d3afc123436e639a6486909ddebdc5ee6ded0d2a
KakudaYutaka <73814373+KakudaYutaka@users.noreply.github.com>
2020-11-26 00:21:29 +0900

    バリデーション設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,10 +3,10 @@ class Address < ApplicationRecord
 
 
   with_options presence: true do
-    validates :postal_code
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'Input correctly' }
     validates :state_id
     validates :city
     validates :block_number
-    validates :phone_number
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'Input only number' }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2020_11_25_142313) do
     t.integer "state_id", null: false
     t.string "city", null: false
     t.string "block_number", null: false
-    t.string "building_name", null: false
+    t.string "building_name"
     t.string "phone_number", null: false
     t.bigint "order_id"
     t.datetime "created_at", precision: 6, null: false
