0a431091ce1511e67e168ac5aaba5ef6d50ecceb
KakudaYutaka <73814373+KakudaYutaka@users.noreply.github.com>
2020-11-25 23:59:34 +0900

    Addressバリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,12 @@
 class Address < ApplicationRecord
   belongs_to :order
+
+
+  with_options presence: true do
+    validates :postal_code
+    validates :state_id
+    validates :city
+    validates :block_number
+    validates :phone_number
+  end
 end
