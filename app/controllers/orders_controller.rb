b3b6b9208bd85f1c0f7155b86a26d04fa0cb4c24
KakudaYutaka <73814373+KakudaYutaka@users.noreply.github.com>
2020-11-26 17:15:18 +0900

    配送先入力時エラー表示&テーブル保存機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,28 @@
 class OrdersController < ApplicationController
+
+  def index
+    @order = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+     if @order_address.valid?
+       @order_address.save
+       redirect_to root_path
+     else
+       render action: :index
+     end
+  end
+
+
+
+
+
+  private
+
+  #クレカの情報も追加すること
+  def order_params
+    params.require(:order_address).permit(:postal_code, :state_id, :city, :block_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,12 +1,3 @@
 class Address < ApplicationRecord
   belongs_to :order
-
-
-  with_options presence: true do
-    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'Input correctly' }
-    validates :state_id
-    validates :city
-    validates :block_number
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'Input only number' }
-  end
 end
